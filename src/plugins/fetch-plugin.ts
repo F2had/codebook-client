7453f24232fd509ce96cf316e7d4d1ca190ad68f
Fahad <42780409+F2had@users.noreply.github.com>
2022-06-11 01:50:41 +0300

    Refactor fetch plugin

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -8,43 +8,59 @@ const fileCache = localforage.createInstance({
 });
 
 export const fetchPlugin = (input: string) => {
-    return {
-        name: 'fetch-plugin',
-        setup(build: esbuild.PluginBuild) {
-        build.onLoad({ filter: /.*/ }, async (args: any) => {
-            console.log('onLoad', args);
-     
-            if (args.path === 'index.js') {
-              console.log('onLoad', input);
-              return {
-                loader: 'jsx',
-                contents: input,
-              };
-            }
-            
-              console.log('onLoad', args);
-    
-              const cachedPackage = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
-    
-              if(cachedPackage) {
-                console.log('Using cache ', args.path);
-                return cachedPackage;
-              }
-    
-              const {data, request} = await axios.get(args.path);
-              
-              const result: esbuild.OnLoadResult =  {
-                loader: 'jsx',
-                contents: data,
-                resolveDir: new URL('.', `${request.responseURL}`).pathname,
-              }
-              console.log('Caching', args.path);
-              await fileCache.setItem(args.path, result);
-    
-              return result;
-            
-            
-          });
+  return {
+    name: 'fetch-plugin',
+    setup(build: esbuild.PluginBuild) {
+
+      build.onLoad({ filter: /.*/ }, async (args: any) => (
+         await fileCache.getItem<esbuild.OnLoadResult>(args.path)
+      ));
+
+      build.onLoad({ filter: /(^index\.js$)/ }, async (args: any) => {
+        return {
+          loader: 'jsx',
+          contents: input
         }
+      });
+
+      build.onLoad({ filter: /\.css$/ }, async (args: any) => {
+        const { data, request } = await axios.get(args.path);
+
+        const contents = 
+        `
+        const style = document.createElement('style');
+        style.innerHTML = ${JSON.stringify(data)};
+        document.head.appendChild(style);
+        `.replace(/\n/g, '');
+
+        const result: esbuild.OnLoadResult =  {
+          loader: 'jsx',
+          contents,
+        }
+    
+
+        await fileCache.setItem(args.path, result);
+        return result;
+      });
+
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+
+
+        const { data, request } = await axios.get(args.path);
+
+
+        const result: esbuild.OnLoadResult = {
+          loader: 'jsx',
+          contents: data,
+          resolveDir: new URL('.', `${request.responseURL}`).pathname,
+        }
+
+        await fileCache.setItem(args.path, result);
+
+        return result;
+
+
+      });
     }
+  }
 };

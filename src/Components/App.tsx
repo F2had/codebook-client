b71b5bf11c323b30632000056ef31547ddd02ba6
Fahad <42780409+F2had@users.noreply.github.com>
2022-06-26 18:00:16 +0300

    Add redux-persist to presist data after refresh

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,18 +1,21 @@
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 import CellList from "./CellList";
 import CodeCell from "./CodeCell";
 import TextEditor from "./TextEditor";
-import store from "../State/store";
+import { persistor, store } from "../State/store";
 
 const App = () => {
   return (
     <Provider store={store}>
-      <>
-        {/* <CodeCell /> */}
-        {/* <TextEditor /> */}
-        <CellList  />
-      </>
+      <PersistGate persistor={persistor}>
+        <>
+          {/* <CodeCell /> */}
+          {/* <TextEditor /> */}
+          <CellList />
+        </>
+      </PersistGate>
     </Provider>
   );
 };
--- src/State/store.ts
+++ src/State/store.ts
@@ -1,17 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' 
 import reducers from "./Reducers";
 
-// import { ActionType } from "./Actions/Types";
+const persistConfig = {
+  key: 'root',
+  storage,
+}
 
+const persistedReducer = persistReducer(persistConfig, reducers)
 
-const store = configureStore({
-  reducer: reducers,
+export const store  =  configureStore({
+  reducer: persistedReducer,
   middleware: [thunk, logger],
 });
 
 
 
-
-export default store;
+export const persistor = persistStore(store);
